18cf956567d76292fb0ede80997e5a6910d919ff
munawar-netsol <munawersheikh@gmail.com>
2021-10-13 08:05:58 +0500

    New API

---
+++ getUser.js
@@ -0,0 +1,22 @@
+const Responses = require('./API_Responses');
+
+exports.handler = async event => {
+    console.log('event', event);    
+    if (!event.pathParameters || !event.pathParameters.ID) {
+        return Responses._400({message: 'Missing the ID from Path!'});
+    }
+    let ID = event.pathParameters.ID;
+    if (data[ID]) {
+        // return data
+        return Responses._200(data[ID]);
+    }
+
+    return Responses._400 ({message: 'No ID in Data'});
+    // Failed as ID not in data.
+}
+
+const data = {
+    123: { name: 'Anna', age: 25, job: 'Journalist' },
+    234: { name: 'Chris', age: 35, job: 'Programmer' },
+    567: { name: 'Tom', age: 35, job: 'Engineer' }
+};
